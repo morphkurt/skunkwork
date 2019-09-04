9a6439636c7803eaed844f624cb7a88501ef51c4
Damitha Gunawardena <damitha.gunawardena+github@gmail.com>
2019-09-04 21:54:29 +1000

    Update plugin-telstra-sign.js

--- brightcove/plugin-telstra-sign.js
+++ brightcove/plugin-telstra-sign.js
@@ -1,21 +1,12 @@
 videojs.registerPlugin('pluginDev', function(options) {
 	var myPlayer = this;
 	var videoId;
-	myPlayer.ready(function(){
-	  videoId = myPlayer.mediainfo.id;
-	  console.log("loaded signing plugin");
-	  console.log(videoId);
-	  myPlayer.catalog.getVideo(videoId, function(error, video){
-		if (error){
-			console.log(error);	
-		}
-		console.log(video);
-	  	for(var i=0;i<video.sources.length;i++){
-      			if(video.sources[i].src){
-        			video.sources[i].src = sign(video.sources[i].src,options);
-      			}
-    	  	}
-    	  	myPlayer.catalog.load(video);
-    	  });
+	myPlayer.one("loadstart", function(){
+		console.log('loadstart')
+		console.log(myPlayer.src())
+		var src = {type: myPlayer.currentType(),  src: sign(myPlayer.src(),options) };
+		console.log(src)
+		myPlayer.src(src)
+		console.log(myPlayer.src())
 	});
 });
