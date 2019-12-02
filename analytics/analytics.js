fdb83f3c1d710b8de1300a5a73885dfb7dc411fa
Damitha Gunawardena <damitha.gunawardena+github@gmail.com>
2019-12-02 19:28:43 +1100

    Update analytics.js

--- analytics/analytics.js
+++ analytics/analytics.js
@@ -12,7 +12,7 @@ videojs.registerPlugin('AdobeConviva', function (options) {
 	if (!options){
 		console.log("Options has not been added, please add the options on video cloud")
 	} else {
-	  prod = (options["prod"] == "true");
+	  prod = (options["env"] == "true");
 	} 
         var myPlayer = this;
         var isContentLoaded = false;
