f69a994af5fc9cb699576d6b112e5f32fe11e430
Damitha Gunawardena <damitha.gunawardena+github@gmail.com>
2019-12-02 15:22:46 +1100

    Create ConvivaCredentials.js

---
+++ analytics/conviva/ConvivaCredentials.js
@@ -0,0 +1,19 @@
+/*! (C) 2016 Conviva, Inc. All rights reserved. Confidential and proprietary. */
+/*! This is sample code meant to illustrate proper Conviva integration in video applications. */
+/*! This file should not be included in video applications as part of integrating Conviva. */
+
+// TODO: replace template values with credentials and custom urls for your accounts
+
+var ConvivaCredentials = {
+
+  production: {
+    customerKey: "cefef1993557ccaad08847f77689343223365e06",
+    gatewayUrl: "https://cws.conviva.com"
+  },
+
+  testing: {
+    customerKey: "da861ec60544e99da6f83ba0361417edf030f2c8",
+    gatewayUrl: "https://conviva.testonly.conviva.com"
+  }
+
+};
