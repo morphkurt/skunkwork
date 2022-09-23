f2502fb5d5402728f9816abb7ad7f45dd8f8c8a5
Damitha Gunawardena <damitha.gunawardena+github@gmail.com>
2022-09-23 19:39:35 +1000

    Update app.js

--- superbook/js/app.js
+++ superbook/js/app.js
@@ -93,6 +93,7 @@ var root = new Vue({
                     })[0]
                     var options = {};
                     var player = videojs('my-player');
+                    let source = sources.filter(s => s.type == "application/x-mpegURL" && s.src.startsWith("https"));
                     let src = source.src.replace('http://', 'https://') + "&secure=true"
                     this.src = source
                     let type = source.type
